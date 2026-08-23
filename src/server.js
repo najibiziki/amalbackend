cc42d9416b4b7b49e2e5fb6cf734f5988933ed44
iz <iz@iz.iz>
2026-08-23 12:22:34 +0100

    vercel.json

--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@ import paymentRouter from "./routes/payment.js";
 import projectRoutes from "./routes/project.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -31,7 +30,4 @@ app.use("/api/instagram", instagramRouter);
 app.use("/api/payment", paymentRouter);
 app.use("/api/projects", projectRoutes);
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+export default app;
